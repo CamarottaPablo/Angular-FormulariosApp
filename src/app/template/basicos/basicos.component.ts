3106d6d7683eaa81bd750393aac498a1018e4eb5
CamarottaPablo <83800798+CamarottaPablo@users.noreply.github.com>
2021-12-02 23:48:14 -0300

    Template: Validar numero mayor a 0

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -21,9 +21,14 @@ export class BasicosComponent implements OnInit {
         && this.miFormulario?.controls.producto?.touched
   }
 
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.value < 0
+        && this.miFormulario?.controls.precio?.touched
+  }
+
   // guardar( miFormulario: NgForm ) {
     guardar(){
-
+      console.log( this.miFormulario )
   }
 
 }
