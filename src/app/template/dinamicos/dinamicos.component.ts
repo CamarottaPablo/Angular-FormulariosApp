5427ebf4999d62bcb004d14a11556dc93a7a628b
CamarottaPablo <83800798+CamarottaPablo@users.noreply.github.com>
2021-12-06 20:02:53 -0300

    Eliminar elemento creado de forma dinamica

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id    : number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  @ViewChild('miFormulario') miFormulario!: NgForm;
+  //  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Pablo',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'Death Stranding'}
+    ]
   }
+
+  eliminar( index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+  
 
   guardar() {
 
